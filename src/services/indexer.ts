0f0207dca3e387c01e2aa2d517337fc70cac0108
Jon Eubank <joneubank@gmail.com>
2021-04-19 12:52:47 -0400

    Indexer uses rollcall, refactored to track index per program

--- src/services/indexer.ts
+++ src/services/indexer.ts
@@ -17,67 +17,162 @@
  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+import PromisePool from '@supercharge/promise-pool';
+
 import logger from '../logger';
 import { getClient } from '../external/elasticsearch';
 import { getAppConfig } from '../config';
 import { FileCentricDocument } from './fileCentricDocument';
 import { File } from '../data/files';
+import getRollcall, { Index } from '../external/rollcall';
 
-const getIndexName = async () => {
-  return (await getAppConfig()).elasticProperties.indexName;
+type ReleaseOptions = {
+  publicRelease: boolean;
 };
+export interface Indexer {
+  updateFile: (file: File) => Promise<void>;
+  indexFiles: (docs: FileCentricDocument[]) => Promise<void>;
+  removeFiles: (docs: FileCentricDocument[]) => Promise<void>;
+  release: (options?: ReleaseOptions) => Promise<void>;
+}
+export const getIndexer = async (options?: { doClone: boolean }) => {
+  // Default doClone to true.
+  const doClone = options ? options.doClone : true;
 
-export async function updateDocFromFile(file: File) {
-  // updates for the file in ES
-  const doc = {
-    embargo_stage: file.embargoStage,
-    release_state: file.releaseState,
+  const rollcall = getRollcall(await getAppConfig());
+
+  type IndexNameHolder = {
+    public: { [programId: string]: Index };
+    restricted: { [programId: string]: Index };
+  };
+  const resolvedIndices: IndexNameHolder = {
+    public: {},
+    restricted: {},
   };
 
-  // TODO: get indexName from rollcall
-  const client = await getClient();
-  await client.update({
-    index: await getIndexName(),
-    id: file.objectId,
-    body: { doc },
-  });
-}
+  const getIndexName = async (program: string, isPublic: boolean): Promise<string> => {
+    const publicIdentifier = isPublic ? 'public' : 'restricted'; // for choosing correct section of resolvedIndices
 
-export async function index(docs: FileCentricDocument[]) {
-  const client = await getClient();
-  const body = docs.map(camelCaseKeysToUnderscore).flatMap(doc => [
-    { update: { _id: doc.object_id } },
-    {
-      doc_as_upsert: true,
-      doc,
-    },
-  ]);
+    const existingIndex: Index | undefined = resolvedIndices[publicIdentifier][program];
+    if (existingIndex) {
+      return existingIndex.indexName;
+    }
 
-  try {
-    await client.bulk({
-      index: await getIndexName(),
-      body,
+    // Index hasn't been fetched yet, lets go grab it.
+    const nextIndex = await rollcall.fetchNextIndex(program, {
+      isPublic,
+      cloneFromReleasedIndex: doClone,
     });
-  } catch (e) {
-    logger.error(`failed bulk indexing request: ${JSON.stringify(e)}`, e);
-    throw e;
+    resolvedIndices[publicIdentifier][program] = nextIndex;
+    return nextIndex.indexName;
+  };
+
+  async function release(options?: ReleaseOptions) {
+    // Default publicRelease to false;
+    const publicRelease = options ? options.publicRelease : false;
+
+    const toRelease = Object.values(resolvedIndices.restricted);
+    if (publicRelease) {
+      toRelease.concat(Object.values(resolvedIndices.public));
+    }
+
+    // TODO: config for max simultaneous release?
+    await PromisePool.withConcurrency(5)
+      .for(toRelease)
+      .handleError((error, index) => {
+        logger.error(`Failed to release index: ${index.indexName}`);
+      })
+      .process(async indexName => {
+        await rollcall.release(indexName);
+      });
+    // Clear stored index names to prevent repeat releases.
+    resolvedIndices.public = {};
+    resolvedIndices.restricted = {};
   }
-}
 
-export async function remove(docs: { [k: string]: any; objectId: string }[]) {
-  const client = await getClient();
-  const body = docs.map(doc => ({ delete: { _id: doc.objectId } }));
+  /**
+   * Update the parts of the file managed by this service:
+   *   - embargo_stage
+   *   - release_state
+   * @param file
+   */
+  async function updateFile(file: File) {
+    // updates for the file in ES
+    const doc = {
+      embargo_stage: file.embargoStage,
+      release_state: file.releaseState,
+    };
 
-  try {
-    await client.bulk({
-      index: await getIndexName(),
-      body,
+    // TODO: get indexName from rollcall
+    const client = await getClient();
+    await client.update({
+      index: await getIndexName(file.programId, false),
+      id: file.objectId,
+      body: { doc },
     });
-  } catch (e) {
-    logger.error(`failed bulk delete request: ${JSON.stringify(e)}`, e);
-    throw e;
   }
-}
+
+  // TODO: Split the docs into programs then bulk index for each program
+  async function indexFiles(docs: FileCentricDocument[]) {
+    const sortedFiles = sortFilesIntoPrograms(docs);
+
+    const client = await getClient();
+
+    // TODO: configure concurrency for ES requests.
+    PromisePool.withConcurrency(20)
+      .for(sortedFiles)
+      .process(async ({ program, files }) => {
+        const index = await getIndexName(program, false);
+        const body = files.map(camelCaseKeysToUnderscore).flatMap(doc => [
+          { update: { _id: doc.object_id } },
+          {
+            doc_as_upsert: true,
+            doc,
+          },
+        ]);
+
+        try {
+          await client.bulk({
+            index,
+            body,
+          });
+        } catch (e) {
+          logger.error(`failed bulk indexing request: ${JSON.stringify(e)}`, e);
+          throw e;
+        }
+      });
+  }
+
+  async function removeFiles(docs: FileCentricDocument[]) {
+    const sortedFiles = sortFilesIntoPrograms(docs);
+
+    const client = await getClient();
+
+    // TODO: configure concurrency for ES requests.
+    PromisePool.withConcurrency(20)
+      .for(sortedFiles)
+      .process(async ({ program, files }) => {
+        const body = docs.map(doc => ({ delete: { _id: doc.objectId } }));
+
+        try {
+          await client.bulk({
+            index: await getIndexName(program, false),
+            body,
+          });
+        } catch (e) {
+          logger.error(`failed bulk delete request: ${JSON.stringify(e)}`, e);
+          throw e;
+        }
+      });
+  }
+
+  return {
+    updateFile,
+    indexFiles,
+    removeFiles,
+    release,
+  };
+};
 
 function camelCaseKeysToUnderscore(obj: any) {
   if (typeof obj != 'object') return obj;
@@ -101,4 +196,26 @@ function camelCaseKeysToUnderscore(obj: any) {
     }
   }
   return obj;
+}
+// Separate list of file documents into distinct list per program.
+type FilesSortedByProgramsArray = Array<{ files: FileCentricDocument[]; program: string }>;
+function sortFilesIntoPrograms(files: FileCentricDocument[]): FilesSortedByProgramsArray {
+  const output: FilesSortedByProgramsArray = [];
+
+  // Sort Files into programs
+  const programMap = files.reduce((acc: { [program: string]: FileCentricDocument[] }, file) => {
+    const program = file.studyId;
+    if (acc[program]) {
+      acc[program].push(file);
+    } else {
+      acc[program] = [file];
+    }
+    return acc;
+  }, {});
+
+  // For each program, add an element to output array
+  Object.entries(programMap).forEach(([program, files]) => {
+    output.push({ program, files });
+  });
+  return output;
 }
